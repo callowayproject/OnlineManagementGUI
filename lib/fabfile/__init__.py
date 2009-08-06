76abad4eda5e79a8577366172b8c433056a8cb33
Corey Oordt <coordt@washingtontimes.com>
2009-08-06 14:19:08 -0400

    Tests for running fabric

--- lib/fabfile/__init__.py
+++ lib/fabfile/__init__.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python
 
-from fabric.api import cd, hide, settings, show, hosts, roles, runs_once, \
-                        require, prompt, put, get, run, sudo, local, env, \
-                        abort, warn
+from fabric.api import *
 from fabric.contrib.console import confirm
 from fabric.contrib.files import exists, first, upload_template, sed, uncomment, \
                         comment, contains, append
 from fabric.contrib.project import rsync_project, upload_project
 
+# We don't need extra output from fabric
+from fabric.state import output
+output.everything = False
+env.warn_only=True
+env.abort=False
+def test():
+    env.host_string = '38.118.71.92'
+    env.user = 'webdev'
+    output = run('ls -l')
+    return output
 
-def install_lib():
-    pass
+
+print test()
